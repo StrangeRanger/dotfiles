6249264a191f2ae2ea7e1fa6fb7ee2cf4f00ecc2
Hunter T. <strangeranger@hthompson.dev>
2025-02-20 21:20:53 -0800

    changed(chezmoi): refactor spacing and style

--- run_before_apply_precompute.bash
+++ run_before_apply_precompute.bash
@@ -6,13 +6,11 @@
 ####[ Global Variables ]################################################################
 
 
-## Used to colorize output.
 C_GREEN="$(printf '\033[0;32m')"
 C_BLUE="$(printf '\033[0;34m')"
 C_NC="$(printf '\033[0m')"
 readonly C_BLUE C_NC
 
-## Short-hand colorized messages.
 readonly C_SUCCESS="${C_GREEN}==>${C_NC} "
 readonly C_INFO="${C_BLUE}==>${C_NC} "
 
@@ -22,35 +20,35 @@ readonly C_INFO="${C_BLUE}==>${C_NC} "
 
 echo "${C_INFO}Running precompute script..."
 
-echo "${C_INFO}    Checking if git-delta is installed..."
+echo "${C_INFO}  Checking if git-delta is installed..."
 # Check if git-delta is installed.
 if command -v delta >/dev/null; then
-    IS_DELTA_INSTALLED=true
+    C_IS_DELTA_INSTALLED=true
 else
-    IS_DELTA_INSTALLED=false
+    C_IS_DELTA_INSTALLED=false
 fi
 
-echo "${C_INFO}    Checking if GUI environment is present..."
+echo "${C_INFO}  Checking if GUI environment is present..."
 # Check for GUI environment. (Specific to Linux)
-if pidof gdm > /dev/null \
-    || pidof lightdm > /dev/null \
-    || pidof sddm > /dev/null \
-    || pidof xorg > /dev/null \
-    || pidof wayland > /dev/null \
+if pidof gdm >/dev/null \
+    || pidof lightdm >/dev/null \
+    || pidof sddm >/dev/null \
+    || pidof xorg >/dev/null \
+    || pidof wayland >/dev/null \
     || [[ -n $DISPLAY ]] \
     || [[ $XDG_SESSION_TYPE == "wayland" ]] \
     || [[ $XDG_SESSION_TYPE = "x11" ]]
 then
-    IS_GUI_ENVIRONMENT=true
+    C_IS_GUI_ENVIRONMENT=true
 else
-    IS_GUI_ENVIRONMENT=false
+    C_IS_GUI_ENVIRONMENT=false
 fi
 
-echo "${C_INFO}    Writing precomputed data to file..."
+echo "${C_INFO}  Writing precomputed data to file..."
 cat <<EOF > "$HOME/.local/share/chezmoi/.precomputed_data.json"
 {
-    "isDeltaInstalled": $IS_DELTA_INSTALLED,
-    "isGUIEnvironment": $IS_GUI_ENVIRONMENT
+    "isDeltaInstalled": $C_IS_DELTA_INSTALLED,
+    "isGUIEnvironment": $C_IS_GUI_ENVIRONMENT
 }
 EOF
 
