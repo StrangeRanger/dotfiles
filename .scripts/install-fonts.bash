b7538aa9d77b68dffeae8fcaeb2d2fe750e9360d
Hunter T. <strangeranger@hthompson.dev>
2025-02-21 15:42:03 -0800

    changed(chezmoi): modify comments and variable style

--- .scripts/install-fonts.bash
+++ .scripts/install-fonts.bash
@@ -15,7 +15,6 @@ C_FONT_FILES=("MesloLGS NF Bold Italic.ttf"
               "MesloLGS NF Regular.ttf")
 readonly C_OS C_FONT_FILES
 
-## Used to colorize output.
 C_YELLOW="$(printf '\033[1;33m')"
 C_GREEN="$(printf '\033[0;32m')"
 C_BLUE="$(printf '\033[0;34m')"
@@ -23,7 +22,6 @@ C_RED="$(printf '\033[1;31m')"
 C_NC="$(printf '\033[0m')"
 readonly C_YELLOW C_GREEN C_BLUE C_RED C_NC
 
-## Short-hand colorized messages.
 readonly C_SUCCESS="${C_GREEN}==>${C_NC} "
 readonly C_WARNING="${C_YELLOW}==>${C_NC} "
 readonly C_ERROR="${C_RED}ERROR:${C_NC} "
@@ -37,22 +35,22 @@ echo "${C_INFO}Installing MesloLGS NF fonts..."
 
 if [[ "$C_OS" == "Darwin" ]]; then
   # Font folder location on macOS.
-  FONT_DIR="$HOME/Library/Fonts"
+  C_FONT_DIR="$HOME/Library/Fonts"
 else
   # Font folder location on Linux.
-  FONT_DIR="$HOME/.local/share/fonts"
+  C_FONT_DIR="$HOME/.local/share/fonts"
 fi
 
 ## Create the font directory if it does not exist.
-[[ ! -d $FONT_DIR ]] && mkdir -p "$FONT_DIR"
+[[ ! -d $C_FONT_DIR ]] && mkdir -p "$C_FONT_DIR"
 
 ## Copy missing font files to the font directory.
 for file in "${C_FONT_FILES[@]}"; do
-    if [[ ! -f "$FONT_DIR/$file" ]]; then
+    if [[ ! -f "$C_FONT_DIR/$file" ]]; then
         echo "${C_WARNING}Missing font: $file"
-        echo "${C_INFO}Copying $file to $FONT_DIR"
-        cp "$HOME/.local/share/chezmoi/.fonts/$file" "$FONT_DIR" || {
-            echo "${C_ERROR}Failed to copy $file to $FONT_DIR"
+        echo "${C_INFO}Copying $file to $C_FONT_DIR"
+        cp "$HOME/.local/share/chezmoi/.fonts/$file" "$C_FONT_DIR" || {
+            echo "${C_ERROR}Failed to copy $file to $C_FONT_DIR"
             exit 1
         }
         font_updated=true
@@ -60,7 +58,7 @@ for file in "${C_FONT_FILES[@]}"; do
 done
 
 ## Update the font cache if any fonts were copied.
-if [[ $font_updated == true ]] && hash fc-cache 2>/dev/null; then
+if [[ $font_updated == true ]] && command -v fc-cache >/dev/null; then
     fc-cache -fv
     echo "${C_SUCCESS}MesloLGS NF fonts installed successfully"
 else
