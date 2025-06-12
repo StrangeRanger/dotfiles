0b6a18affca27689008e21a814d6025212b76d9b
Hunter T. <strangeranger@hthompson.dev>
2025-06-11 19:47:20 -0700

    changed(chezmoi): modify font files use + more

--- .scripts/install-fonts.bash
+++ .scripts/install-fonts.bash
@@ -2,17 +2,18 @@
 #
 # Install MesloLGS NF fonts.
 #
-########################################################################################
-####[ Global Variables ]################################################################
+############################################################################################
+####[ Global Variables ]####################################################################
 
 
 font_updated=false
 
 C_OS=$(uname -s)
-C_FONT_FILES=("MesloLGS NF Bold Italic.ttf"
-              "MesloLGS NF Bold.ttf"
-              "MesloLGS NF Italic.ttf"
-              "MesloLGS NF Regular.ttf")
+# Modify with the your prefered Meslo Nerd Font files, from within the .fonts directory.
+C_FONT_FILES=("MesloLGMNerdFont-Bold.ttf"
+    "MesloLGMNerdFont-BoldItalic.ttf"
+    "MesloLGMNerdFont-Italic.ttf"
+    "MesloLGMNerdFont-Regular.ttf")
 readonly C_OS C_FONT_FILES
 
 C_YELLOW="$(printf '\033[1;33m')"
@@ -28,7 +29,7 @@ readonly C_ERROR="${C_RED}ERROR:${C_NC} "
 readonly C_INFO="${C_BLUE}==>${C_NC} "
 
 
-####[ Main ]############################################################################
+####[ Main ]################################################################################
 
 
 if [[ "$C_OS" == "Darwin" ]]; then
