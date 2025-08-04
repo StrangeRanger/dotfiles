23bfda06887c73f10ada171899aa791cc6a36f12
Hunter T. <strangeranger@hthompson.dev>
2025-08-04 15:15:54 -0700

    fixed(chezmoi): add missing color variables

--- .scripts/install-fonts.bash
+++ .scripts/install-fonts.bash
@@ -19,6 +19,7 @@ readonly C_OS C_FONT_FILES
 C_YELLOW="$(printf '\033[1;33m')"
 C_GREEN="$(printf '\033[0;32m')"
 C_BLUE="$(printf '\033[0;34m')"
+C_CYAN="$(printf '\033[0;36m')"
 C_RED="$(printf '\033[1;31m')"
 C_NC="$(printf '\033[0m')"
 readonly C_YELLOW C_GREEN C_BLUE C_RED C_NC
@@ -27,6 +28,7 @@ readonly C_SUCCESS="${C_GREEN}==>${C_NC} "
 readonly C_WARNING="${C_YELLOW}==>${C_NC} "
 readonly C_ERROR="${C_RED}ERROR:${C_NC} "
 readonly C_INFO="${C_BLUE}==>${C_NC} "
+readonly C_NOTE="${C_CYAN}==>${C_NC} "
 
 
 ####[ Main ]################################################################################
